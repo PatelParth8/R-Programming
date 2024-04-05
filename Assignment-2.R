d91c8889514d312a57c99484ab696a9d5e4b023f
Patel Parth <138985889+PatelParth8@users.noreply.github.com>
2024-04-05 18:17:43 +0530

    Add files via upload

--- Assignment-2.R
+++ Assignment-2.R
@@ -1,108 +1,198 @@
-#ASSIGNMENT 2
+#SET 1
+# 1
 
-#Que :- 1 Write a R program to create three vectors numeric data, character data and logical data. Display the content of the vectors and their type.
-a <- c(1,2,3,4,5)
-b <- c("A", "B", "C", "D", "E")
-c <- c(TRUE)
+Selling_of_Machines <- c(45,95,105,75,85,80,90,65,110,100)
+City <- c("Bhavnagar", "Ahmedabad", "Pune", "Bengaluru", "Chennai", "Mumbai", "Mathura", "Vadodara", "Delhi", "Shimla")
 
-cat("Value of A :",a)
-cat("\nValue of B :",b)
-cat("\nValue of C :",c)
+df <- data.frame(Selling_of_Machines, City)
+print(df)
 
-cat("\n Data Type of A :",class(a))
-cat("\n Data Type of B :",class(b))
-cat("\n Data Type of C :",class(c))
+hist(df$Selling_of_Machines, labels = df$City, main = "Histogram",xlab = "Selling of Machines", col = "yellow", border = "red", xlim = c(60, 100))
+legend(x = "topright", legend = "Selling of Machines", fill = "yellow")
 
-#Que :- 2 Write a menu driven R program that perform addition, subtraction, multiplication and division of two vectors.
-print("Enter 1 for Addition")
-print("Enter 2 for Subtraction")
-print("Enter 3 for Multiplication")
-print("Enter 4 for Division")
 
-n1 <- c(1,2,3)
-n2 <- c(4,5,6)
-input <- readline("Enter the Number of Your Choice : ")
-cat("Entered Number is",input)
-as.numeric(input)
+# 2
 
-add <- n1 + n2
-sub <- n1 - n2
-mul <- n1 * n2
-div <- n1 / n2
+v <- c(14,76,56,34,23,89,78,10)
+print(v)
 
-if(input == 1){
-  cat("\nAddition is",add)
-}else if(input == 2){
-  cat("\nSubtraction is",sub)
-}else if(input == 3){
-  cat("\nMultiplication is",mul)
-}else if(input == 4){
-  cat("\nDivision is",div)
-}else{
-  print("Enter the Valid Number")
-}
+# Method 1
+z <- (v - mean(v)) / sd(v)
+print(z)
 
-#Que :- 3 Write a R program to find Sum, Mean and Product of a Vector.
-a <- c(1,2,3,4)
-cat("Value of Vector A is",a)
+# Method 2
+print(scale(v, scale = TRUE, center = TRUE))
 
-cat("\nSum is",sum(a))
-cat("\nMean is",mean(a))
-cat("\nProduct is",prod(a))
+# 3
 
-#Que :- 4 Write a R program to find the minimum and the maximum of a Vector.
-a <- c(10,20,30,40)
-cat("Value of A is",a)
+v1 <- c(12,13,15,17,16,19,20,23,25,27,29,31,NA)
+print(v1)
 
-cat("\nMinimum of Vector A is",min(a))
-cat("\nMaximum of Vector A is",max(a))
+mean <- mean(v1, na.rm = TRUE)
+print(mean)
 
-#Que :- 5 Write a R program to sort a Vector in ascending and descending order.
-a <- c(1,4,20,3,15)
-cat("Value of Vector A is",a)
+median <- median(v1, na.rm = TRUE)
+print(median)
 
-cat("\nAscending Order :",sort(a))
-cat("\nDescending Order :",sort(a, decreasing = TRUE))
+mode <- names(sort(table(v1)))
+print(mode)
 
-#Que :- 6 Write a R program to test whether a given vector contains a specified element entered by user.
-a <- c(1,2,3,4)
-cat("Value of Vector A is",a)
-input <- readline("Enter the Value to Find : ")
-cat("\nEntered Value is",input)
+mode <- names(sort(table(v1)))[1]
+print(mode)
 
-if(input %in% a){
-  cat("\nValue Found in Vector A")
-}else{
-  cat("\nValue Not Found in Vector A")
-}
+mul <- v1 * 2
+print(mul)
 
-#Que :- 7 Write a R program to find the elements of a given vector that are not in another given vector.
-a <- c(1,3,5,7,9)
-b <- c(9,8,6,5,3)
+print(length(v1))
 
-cat("Elements of A :",a)
-cat("\nElement of B :",b)
-cat("\nElements Present in A not in B are",setdiff(a,b))
+a <- 45
+v1 <- c(v1, a)
+print(v1)
 
-#Que :- 8 Write a R program to combines two given vectors by columns and by rows.
-a <- c(1,2,3)
-b <- c(4,5,6)
+print(sort(v1 + 5, decreasing = TRUE))
 
-cat("Elements of A :",a)
-cat("\nElements of B :",b)
-cat("\nRow Bind Vector is",rbind(a,b))
-cat("\nColumn Bind Vector is",cbind(a,b))
+# 4
 
-#Que :- 9 Write a R program to list the distinct values in a vector from a given vector.
-a <- c(1,3,5,3,7,10,25,10,50,25)
+data <- wilcox.test(mpg ~ am, data = mtcars)
+print(data$p.value)
 
-cat("Elements of Vector A is",a)
-cat("\nUnique Elements Are",unique(a))
+# SET : 2
 
-#Que :- 10 Write a R program to combine value from two vector in one.
-a <- c(1,3,5,7,9)
-b <- c(2,3,6,8,0)
+# 1
+Selling_of_LEDs <- c(69,93,110,65,75,80,97,43,55)
+Selling_of_LCDs <- c(61,78,100,53,69,80,79,39,48)
 
-cat("Elements of Vector A are",a)
-cat("\nElements of Vector B are",b)
-cat("\nUnion is",union(a,b))
+df <- data.frame(Selling_of_LEDs, Selling_of_LCDs)
+print(df)
+
+boxplot(df$Selling_of_LEDs, df$Selling_of_LCDs, main = "Box Plot", col = c("blue", "yellow"), xlab = "Selling_of_LEDs", ylab = "Selling_of_LCDs")
+legend(x = "topright", legend = c("Selling_of_LEDs", "Selling_of_LCDs"), fill = c("blue", "yellow"))
+
+# 2
+V1 <- c(124,176,116,134,219,189,178,310)
+V2 <- c(110,120,180,200,160,155,168,210)
+
+t.test(V1, mu = 134)
+t.test(V1, V2, var.equal = TRUE)
+t.test(V1, V2, paired = TRUE)
+
+# 3
+v <- c(112,139,195,172,162,119,2202,23,225,217,219,131,NA)
+
+mean <- mean(v, na.rm = TRUE)
+print(mean)
+
+median <- median(v1, na.rm = TRUE)
+print(median)
+
+mode <- names(sort(table(v)))
+print(mode)
+
+mode <- names(sort(table(v)))[1]
+print(mode)
+
+maximum <- max(v, na.rm = TRUE)
+print(maximum)
+
+minimum <- min(v, na.rm = TRUE)
+print(minimum)
+
+print(length(v))
+
+a <- 345
+v <- c(v, a)
+print(v)
+
+print(sort(v + 5, decreasing = TRUE))
+
+
+# 3
+mileage <- mtcars$mpg
+print(mileage)
+
+weight <- mtcars$wt
+print(weight)
+
+correlation <- cor(mileage, weight, method = "pearson")
+print(correlation)
+
+a <- lm(weight~mileage)
+print(a)
+
+plot(mileage, weight, main = "Plot", xlab = "Mileage", ylab = "Weight", abline(lm(weight~mileage)), col = "blue", cex = 1.5, pch = 20)
+legend(x = "topright", legend = c("Mileage", "Weight"), fill = c("blue"))
+
+
+# SET : 3
+
+# 1
+Height <- c(165,169,168,159,175,180,186)
+print(Height)
+
+Weight <- c(45,48,47,43,59,65,69)
+print(Weight)
+
+df <- data.frame(Height, Weight)
+print(df)
+
+plot(df$Height, df$Weight, main = "Scatter Plot", xlab = "Height", ylab = "Weight", abline(lm(Weight~Height)), col = "red", cex = 1.5, pch = 20)
+
+# 2
+input <- mtcars[,c('mpg', 'hp', 'am')]
+print(input)
+
+a <- aov(mpg ~ hp * am, data = mtcars)
+print(summary(a))
+
+b <- aov(mpg ~ hp + am, data = mtcars)
+print(summary(b))
+
+# 3
+v <- c(11,13,15,12,14,19,22,23,25,21,29,31,NA)
+print(v)
+
+mean <- mean(v, na.rm = TRUE)
+print(mean)
+
+median <- median(v, na.rm = TRUE)
+print(median)
+
+mode <- names(sort(table(v)))
+print(mode)
+
+mode <- names(sort(table(v)))[1]
+print(mode)
+
+mul <- v * 3
+print(mul)
+
+print(length(v))
+
+a <- 345
+v <- c(v, a)
+print(v)
+
+print(sort(v + 5, decreasing = TRUE))
+
+# 4
+input <- mtcars[,c('mpg', 'disp', 'hp', 'wt')]
+print(input)
+
+model <- lm(mpg ~ disp + hp + wt, data = mtcars)
+print(model)
+print(summary(model))
+
+xall <- coef(model)[1]
+print(xall)
+
+xdisp <- coef(model)[2]
+print(xdisp)
+
+xhp <- coef(model)[3]
+print(xhp)
+
+xwt <- coef(model)[4]
+print(xwt)
+
+a <- xall + xdisp + xhp + xwt
+print(a)
